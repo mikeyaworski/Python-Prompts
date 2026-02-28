99cc43d77b9d28c025e0a0487a1264193c2eda50
Michael Yaworski <mikeyaworski@hotmail.com>
2026-02-27 23:45:24 -0500

    Fix: Keybind state management
    With the update to the forked InquirerPy, the state API changed
    6ef600f7db5afb7aafbdda5b452bf5b539598070

--- src/prompts/keybinds.py
+++ src/prompts/keybinds.py
@@ -12,42 +12,23 @@ def register_delete_keybind(prompt):
   def _(event):
     if event.app.current_buffer.text == '':
       event.app.exit(result=None)
-    event.app.current_buffer.text = ''
-    event.app.current_buffer.cursor_position = 0
+    else:
+      event.app.current_buffer.text = ''
+      event.app.current_buffer.cursor_position = 0
 
 def register_left_keybind(prompt):
   '''Deselect the current option'''
   @prompt.register_kb('left')
   def _(event):
-    if len(prompt.content_control._filtered_choices) > 0:
-      current_selected_index = prompt.content_control.selection['index']
-      prompt.content_control.choices[current_selected_index]['enabled'] = False
-    # We could allow cursor repositioning with arrow keys for the fuzzy search,
-    # but it's really pointless in practice since users would just backspace the input to adjust.
-    # It feels clunky to sometimes change the selection with arrow and sometimes move the cursor.
-    # buffer = event.app.current_buffer
-    # if buffer.text == '':
-    #   current_selected_index = prompt.content_control.selection['index']
-    #   prompt.content_control.choices[current_selected_index]['enabled'] = False
-    # if buffer.cursor_position > 0:
-    #   buffer.cursor_position -= 1
+    if prompt.content_control.choice_count > 0:
+      prompt._set_choice_enabled(prompt.content_control.selection['index'], False)
 
 def register_right_keybind(prompt):
   '''Select the current option'''
   @prompt.register_kb('right')
   def _(event):
-    if len(prompt.content_control._filtered_choices) > 0:
-      current_selected_index = prompt.content_control.selection['index']
-      prompt.content_control.choices[current_selected_index]['enabled'] = True
-    # We could allow cursor repositioning with arrow keys for the fuzzy search,
-    # but it's really pointless in practice since users would just backspace the input to adjust.
-    # It feels clunky to sometimes change the selection with arrow and sometimes move the cursor.
-    # buffer = event.app.current_buffer
-    # if buffer.text == '':
-    #   current_selected_index = prompt.content_control.selection['index']
-    #   prompt.content_control.choices[current_selected_index]['enabled'] = True
-    # if buffer.cursor_position < len(buffer.text):
-    #   buffer.cursor_position += 1
+    if prompt.content_control.choice_count > 0:
+      prompt._set_choice_enabled(prompt.content_control.selection['index'], True)
 
 def register_fuzzy_search_enter_keybind(prompt):
   '''
@@ -65,10 +46,8 @@ def register_fuzzy_search_enter_keybind(prompt):
     elif not buffer.text and choices_selected:
       prompt._handle_enter(event)
     elif buffer.text:
-      if len(prompt.content_control._filtered_choices) > 0:
-        selection = prompt.content_control.selection
-        current_selected_index = selection['index']
-        prompt.content_control.choices[current_selected_index]['enabled'] = True
+      if prompt.content_control.choice_count > 0:
+        prompt._set_choice_enabled(prompt.content_control.selection['index'], True)
       event.app.current_buffer.text = ''
       event.app.current_buffer.cursor_position = 0
 
